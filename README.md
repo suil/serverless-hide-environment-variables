9b90bae443566e4250dc99570749941d77ca8be9
Shuang Liu <shuang.liu@morningstar.com>
2019-04-25 14:56:08 -0500

    initial check in

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# serverless-hide-environment-variables
-Serverless plugin that hides environment variables for functions before deployment
+# Serverless plugin to hide environment variables
+Serverless plugin that hides environment variables for AWS Lambda functions using AWS Key Management Service (KMS) before deployment
+
+## Introduction
+Checking-in environment variables in serverless.yml into repositories is a very convenient and popular way to manage runtime configurations for serverless applications. With this approach, however, sensitive information like API keys and secret tokens will be exposed publicly. This pratice is probably against security policies in every organization.
+
+This plgin is to hide sensitive environment variables in both provider's environment section and functions's environment section using KMS encrypted string. Then, the serverless framework's deployment process will decrypt them using KMS keys in a specified region, so that Lambda function can consume them from `process.env.SECRET_API_KEY` in the runtime.
+
+## Usage
+
+### Encrypt your sensitive environment variable using AWS KMS service
+Follow the instruction https://docs.aws.amazon.com/cli/latest/reference/kms/encrypt.html to encrypt your sensitive environment variable using a KMS key from a region.
+
+### Add the environment variable using this format
+Put your encrypted string to the provider's environment section or function's environment session in serverless.yml in this following format:
+
+```
+provider:
+  ...
+  environment:
+    GENERAL_ENCRYPTED_API_KEY:
+      encrypted: AQICAHinIKhx8yV+y97+qS5naGEB...
+      kmsKeyRegion: us-east-1
+    GENERAL_NORMAL_API_KEY: unencrypted-key-value
+
+functions:
+  yourLambdaFunction:
+    environment:
+      FUNCTION_SPECIFIC_ENCRYPTED_API_KEY:
+        encrypted: AQICAHinIKhx8yV+y97+qS5naGEB...
+        kmsKeyRegion: us-west-2
+      FUNCTION_SPECIFIC_NORMAL_API_KEY: unencrypted-key-value
+
+plugins:
+  - serverless-hide-environment-variables
+  ...
+```
+
+If an environment variable is a encrypted string, it has to be put as an object that has `encrypted` key. `kmsKeyRegion` key in the object is the region where the KMS key you use for encrypting the variable value. `kmsKeyRegion` key is optional. If it's missing, the region from command line will be used. If the region from command line is even missing, `'us-east-1'` will be used.
+
+### Local invocation of a lambda function.
+The decryption does actually work in the local invocation for a lambda function. Once the environment variables are configured correctly, `process.env.SECRET_API_KEY` will have decrypted value as if it's in deployed Lambda environtment.
+
+    
+
---
+++ index.js
@@ -0,0 +1,77 @@
+const AWS = require('aws-sdk');
+
+module.exports = class HideEnvironmentVariablesPlugin {
+    constructor(serverless, options) {
+        this.serverless = serverless;
+        this.region = options.region;
+        this.options = options;
+        this.hooks = {
+            'before:deploy:functions': this.replaceEnvironmentVariables.bind(this),
+            'package:createDeploymentArtifacts': this.replaceEnvironmentVariables.bind(this),
+            'before:deploy:function:packageFunction': this.replaceEnvironmentVariables.bind(this),
+            'before:invoke:local:invoke': this.replaceProcessEnvVariablesForFunction.bind(this),
+            'before:invoke:local:loadEnvVars': this.replaceEnvironmentVariables.bind(this)
+        };
+    }
+
+    async awsKmsDecrypt(value, region) {
+        try {
+            const kms = new AWS.KMS({ apiVersion: '2014-11-01', region });
+            const result = await kms.decrypt({ CiphertextBlob: new Buffer(value, 'base64') }).promise();
+            return result.Plaintext.toString('ascii');
+        } catch (error) {
+            this.serverless.cli.consoleLog('AWS KMS service cannot decrypt');
+            throw error;
+        }
+    }
+
+    async decryptVariable(environmentVariable) {
+        if (typeof environmentVariable !== 'object' || !environmentVariable.encrypted) {
+            return environmentVariable;
+        }
+        const { encrypted, kmsKeyRegion } = environmentVariable;
+        const decrypted = await this.awsKmsDecrypt(encrypted, kmsKeyRegion || this.region || 'us-east-1');
+        return decrypted;
+    }
+    
+    async replaceVariables(environmentVariables) {
+        if (!environmentVariables) {
+            return;
+        }
+        return Promise.all(
+            Object.keys(environmentVariables)
+                .filter(variableName => typeof environmentVariables[variableName] === 'object' && environmentVariables[variableName].encrypted)
+                .map(async variableName => {
+                    environmentVariables[variableName] = await this.decryptVariable(environmentVariables[variableName]);
+                    this.serverless.cli.consoleLog(`Decrypted variable ${variableName} to ${environmentVariables[variableName]}`);
+                })
+        );
+    }
+    
+    async replaceEnvironmentVariables() {
+        return Promise.all([
+            this.replaceVariables(this.serverless.service.provider.environment),
+            ...Object.keys(this.serverless.service.functions).map(async func => {
+                await this.replaceVariables(this.serverless.service.functions[func].environment);
+            })]
+        );
+    }
+
+    replaceProcessEnvVariablesForFunction() {
+        // replace shared provider environment variables
+        if (this.serverless.service.provider.environment) {
+            Object.keys(this.serverless.service.provider.environment).forEach(envVarName => {
+                process.env[envVarName] = this.serverless.service.provider.environment[envVarName];
+            });
+        }
+        
+        const func = this.options.function;
+        // replace function's environment variables
+        const funcConfig = this.serverless.service.functions[func];
+        if (funcConfig && funcConfig.environment) {
+            Object.keys(funcConfig.environment).forEach(envVarName => {
+                process.env[envVarName] = funcConfig.environment[envVarName];
+            });
+        }
+    }
+};
